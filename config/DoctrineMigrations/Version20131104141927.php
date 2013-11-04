fd65e0d40b7857ea192e26b140bb144bee763863
Vic Cherubini <vcherubini@leftnode.com>
2013-11-04 14:20:46 -0600

    Initial commit of the DoctrineMigrations directory for making small migrations.

---
+++ config/DoctrineMigrations/Version20131104141927.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+class Version20131104141927 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+
+    }
+
+    public function down(Schema $schema)
+    {
+
+    }
+}
